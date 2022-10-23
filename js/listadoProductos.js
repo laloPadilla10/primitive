94cb5a80c6cc4918764324f41fa8bca3b8ed2906
Margarita Villaseñor <maggie.villasenor@hotmail.com>
2022-10-23 00:15:50 -0500

    fix: problem with the product list display

--- js/listadoProductos.js
+++ js/listadoProductos.js
@@ -1,106 +1,91 @@
-function addItem(item){
-    const itemHTML = /*'<div class="card-group">\n'+
-    '<div class="card border-info" style="width: 18rem;">\n' +
-        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-header">'+item.name+'</h5>\n' +
-        '        <p class="card-text">'+item.description+'</p>\n' +
-        '        <a href="#" class="btn btn-primary">Add</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-    '</div>\n' +
-        '<br/>';*/
-
-/* <div class=" " style="display:flex;justify-content:center;box-sizing:border-box">
-  <div class="col-sm">
-    <div class="card border-info">
-      <img src="${item.img}" class="card-img-top" alt="${item.name}">
-      <div class="card-body">
-        <h5 class="card-header">${item.name}</h5>
-        <p class="card-text">${item.description}</p>
-        <a href="#" class="btn btn-info"><i
-        class="fa-solid fa-cart-shopping"></i></a>
-      </div>
-    </div>
-  </div> */
-
-  ` <div class="card">
-        <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
-            data-mdb-ripple-color="light">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/belt.webp"
-              class="w-100" />
-            <a href="#!">
-              <div class="mask">
-                <div class="d-flex justify-content-start align-items-end h-100">
-                  <h5><span class="badge bg-primary ms-2">New</span></h5>
-                </div>
-              </div>
-              <div class="hover-overlay">
-                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-              </div>
-            </a>
-          </div>
-          <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
-            </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
-            </a>
-            <h6 class="mb-3">$61.99</h6>
+function addItem(item) {
+  const itemHTML =
+    `<div class="col-md-4 col-sm-6 col-xs-12" id="card" style=" padding-bottom:20px;" >
+      <div class="card h-100" >
+        <div class="card-header">
+          <img class="card-img" src="${item.img}" alt="${item.name}" style="height: 322px;">
+          <div class="card-img-overlay  justify-content-end">
+          <a href="#" class="card-link text-danger like">
+          <i class="fas fa-heart"></i>
+          </a>
           </div>
         </div>
-      
-      `
-      
-        
+        <div class="card-body">
+          <h4 class="card-title">${item.name}</h4>
+          <p class="card-text">${item.description}</p>
+        </div>
+        <div class=" card-footer buy d-flex justify-content-between align-items-center">
+          <div class="price text-"><h5 class="mt-4">$125</h5></div>
+          <a href="#" class="btn btn-danger"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+    </div> `;
 
-    
-        
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
+  const itemsContainer = document.getElementById("list-items");
+  itemsContainer.innerHTML += itemHTML;
 }
 
-addItem(
-    {
-        'name':'Camiseta Cobra Kai',
-        'img':'https://i.ibb.co/Qf3JqnS/Cobra-Kai-Strike-First-Unisex-T-Shirt.jpg',
-        'description':'Una camiseta Cobra Kai cómoda con mangas cortas y cuello redondo. ¡Perfecto para usar mientras viaja o descansando adentro!'
-    }
-    )
+addItem({
+  name: "Camiseta Cobra Kai",
+  img: "https://i.ibb.co/Qf3JqnS/Cobra-Kai-Strike-First-Unisex-T-Shirt.jpg",
+  description:
+    "Una camiseta Cobra Kai cómoda con mangas cortas y cuello redondo. ¡Perfecto para usar mientras viaja o descansando adentro!",
+});
 
-addItem({'name':'Camiseta unisex Maestro Roshi de Dragon Ball',
-    'img':'https://i.ibb.co/yVsRYw8/maestro-Roshi.jpg',
-    'description':'Producto con licencia oficial de Dragon Ball.'});
+addItem({
+  name: "Camiseta unisex Maestro Roshi de Dragon Ball",
+  img: "https://i.ibb.co/yVsRYw8/maestro-Roshi.jpg",
+  description: "Producto con licencia oficial de Dragon Ball.",
+});
 
-addItem({'name':'Sudadera con gorro Gamer unisex',
-    'img':'https://i.ibb.co/mrKs152/Game-Stop-Gamer-Unisex-Hooded-Sweatshirt.jpg',
-    'description':'Una sudadera con gorro que ofrece comodidad y calidad premium'})
+addItem({
+  name: "Sudadera con gorro Gamer unisex",
+  img: "https://i.ibb.co/mrKs152/Game-Stop-Gamer-Unisex-Hooded-Sweatshirt.jpg",
+  description: "Una sudadera con gorro que ofrece comodidad y calidad premium",
+});
 
-addItem({'name':'Sudadera con gorro StarWars unisex',
-    'img':'https://i.ibb.co/WGgyL5F/categoria-ropa-opcion-2.png',
-    'description':'Una sudadera con gorro que ofrece comodidad y calidad premium'})
+addItem({
+  name: "Sudadera con gorro StarWars unisex",
+  img: "https://i.ibb.co/WGgyL5F/categoria-ropa-opcion-2.png",
+  description: "Una sudadera con gorro que ofrece comodidad y calidad premium",
+});
 
-addItem({'name':'Batman: The Killing Joke Deluxe',
-    'img':'https://i.ibb.co/HTjBY7m/batman-the-killing-joke-deluxe.jpg',
-    'description':'En Batman: The Killing Joke, The Joker, toma el origen del mayor supervillano de los cómics y cambia el mundo de Batman para siempre.'});
+addItem({
+  name: "Batman: The Killing Joke Deluxe",
+  img: "https://i.ibb.co/HTjBY7m/batman-the-killing-joke-deluxe.jpg",
+  description:
+    "En Batman: The Killing Joke, The Joker, toma el origen del mayor supervillano de los cómics y cambia el mundo de Batman para siempre.",
+});
 
-addItem({'name':'Tamagotchi R2-D2',
-    'img':'https://i.ibb.co/w4nHyXW/starwars-Tamagotchi.webp',
-    'description':'Cuida de R2-D2 manteniéndolo cargado, limpio y jugando con él.'});
+addItem({
+  name: "Tamagotchi R2-D2",
+  img: "https://i.ibb.co/w4nHyXW/starwars-Tamagotchi.webp",
+  description: "Cuida de R2-D2 manteniéndolo cargado, limpio y jugando con él.",
+});
 
-addItem({'name':'Nintendo Switch Lite Console Coral',
-    'img':'https://i.ibb.co/Xjr9nPh/Nintendo-Switch-Lite-Console-Coral.jpg',
-    'description':'Presentamos Nintendo Switch™ Lite, una nueva versión del sistema Nintendo Switch que está optimizada para el juego portátil personal.'});
+addItem({
+  name: "Nintendo Switch Lite Console Coral",
+  img: "https://i.ibb.co/Xjr9nPh/Nintendo-Switch-Lite-Console-Coral.jpg",
+  description:
+    "Presentamos Nintendo Switch™ Lite, una nueva versión del sistema Nintendo Switch que está optimizada para el juego portátil personal.",
+});
 
-addItem({'name':'Figura oficial de Sideshow Collectibles Star Wars C-3PO escala 1:6',
-    'img':'https://i.ibb.co/ZcPzjw4/c3po-figure.png',
-    'description':'La figura de C-3PO a escala de 6 pulgadas se detalla para parecerse al personaje de la pelicula Star Wars, con detalles de primera calidad y múltiples puntos de articulación.'});
+addItem({
+  name: "Figura oficial de Sideshow Collectibles Star Wars C-3PO escala 1:6",
+  img: "https://i.ibb.co/ZcPzjw4/c3po-figure.png",
+  description:
+    "La figura de C-3PO a escala de 6 pulgadas se detalla para parecerse al personaje de la pelicula Star Wars, con detalles de primera calidad y múltiples puntos de articulación.",
+});
 
-addItem({'name':'Figura Oficial Furyu Hatsune Miku Cat Auriculares Versión 1/7 25cm (9.8")',
-    'img':'https://i.ibb.co/Vmqh6C6/heo-hm-cehf11-1024x1024.webp',
-    'description':'Producto con licencia oficial de Hatsune Miku. '});
+addItem({
+  name: 'Figura Oficial Furyu Hatsune Miku Cat Auriculares Versión 1/7 25cm (9.8")',
+  img: "https://i.ibb.co/Vmqh6C6/heo-hm-cehf11-1024x1024.webp",
+  description: "Producto con licencia oficial de Hatsune Miku. ",
+});
 
-addItem({'name':'Robotech®: El juego de rol de Macross Saga',
-    'img':'https://i.ibb.co/R0R8Ggz/Robotech-3-D-Blank-Book-Cover-3.jpg',
-    'description':'Robotech: Macross Saga consta de 264 páginas de asombrosa calidad artística. Los fanáticos de Robotech no se sentirán decepcionados.'});
+addItem({
+  name: "Robotech®: El juego de rol de Macross Saga",
+  img: "https://i.ibb.co/R0R8Ggz/Robotech-3-D-Blank-Book-Cover-3.jpg",
+  description:
+    "Robotech: Macross Saga consta de 264 páginas de asombrosa calidad artística. Los fanáticos de Robotech no se sentirán decepcionados.",
+});
