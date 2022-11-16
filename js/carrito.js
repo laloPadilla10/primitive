84a1705712fb56a09445ded41e7a90b98bd90120
Anahi Parrilla <parrilla2552@gmail.com>
2022-11-15 20:21:17 -0600

    fix: table carrito

--- js/carrito.js
+++ js/carrito.js
@@ -3,22 +3,11 @@ function cargarCarrito() {
   let main = document.getElementById("main");
   main.innerHTML =
 
-    `<main class="container-fluid">
-  <page-title class="row justify-content-center my-3">
-  <!-- <div class="col text-center">
-      <h1>Mi Carrit UwU</h1>
-    </div> -->
-  </page-title>
-
-  <section class="row m-2">
-    <!--- Artículos del carrito --->
-    <section class="col-sm-12 mb-2 my-2">
-      <!--- Elemento en carrito -->
-
-          <div <h2 class="text-center">Tu carrito</h2></div>
-          <div class="text-center">
-          <div class="row">
-            <table>
+    `<div class="container  text-center">
+       <div class="row">
+         <div class="col col-md-2"></div>
+           <div class="col col-md-8">
+             <table class="table">
               <thead>
                 <tr>
                   <th>Imagen</th>
@@ -28,29 +17,28 @@ function cargarCarrito() {
                   <th>Subtotal</th>
                   <th>Borrar</th>
                 </tr>
-              </thead>
+               </thead>
               <tbody id="body"></tbody>
             </table>
-            <hr>
+           </div>
+          <div class="col col-md-2"></div>
+       </div>
+    </div>
+           <hr>
             <div id="TotalCarrito" class="col-sm-7 my-auto text-center" style="display:none">
             </div>
             
             <div class="col-sm-3 text-center my-2">
               <button href="#" class="btn btn-light" ${arregloCarrito.length==0 ? "disabled" : ""}> Pagar </button>
             </div>
-          </div>
+  
           <div class="row">
               <div class="col text-center mt-4">
                   <small class="text-muted">
                   ¿Tienes un problema con tu compra? ¡Contáctanos!
-              </small>
-                </div>
-          </div>
-        </div>
-      </div>
-      <!--- Carta con botón de pago-->
-      
-    </section>`;
+                   </small>
+              </div>
+          </div>`
   carritoPrueba();
 };
 
@@ -61,7 +49,6 @@ let remove = obj => {
   arregloCarrito.splice($(obj).parent().parent().index(),1); //elimina el elemento del arreglo carrito. 
   $(obj).parent().parent().remove(); //elimina la fila de mi tabla. 
 }
-
 
 
 //esta función lee los elementos de mi carrito
